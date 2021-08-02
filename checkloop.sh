24dc22deeff7c2ea1fd9f7997ac00837a3178e82
Miguel Angel Murciego <miguelangel.murciego@mrm.com>
2021-08-02 15:00:10 +0200

    fix

--- checkloop.sh
+++ checkloop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+printf "last check on `date`\n" >> checkloop.log
 node pup.js "$1" |grep "available: true" && true
 
 while [ "$?" != "0" ]
 do
 sleep "$[($RANDOM % 120) + 1]"
-printf "last check on `date`/n" >> checkloop.log
+printf "last check on `date`\n" >> checkloop.log
 node pup.js "$1" |grep "available: true" && true
 done
 
